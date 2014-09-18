c5bdba890e3fe4e1ba76f0673d7ba93b90fe5065
Matthew Treinish <mtreinish@kortar.org>
2014-09-17 23:36:56 -0400

    Add mysql-proxy to enable read-only access to a db
    This commit adds a mysql_proxy module which will setup a read-only
    proxy to a mysql db. This also configures a proxy to the subunit2sql
    db to run on logstash.o.o to provide read only access to the data in
    the database.
    
    Change-Id: I478baca354354347fe50074a8e3b9f66ca890d55

---
+++ openstack_project/manifests/jenkins.pp
@@ -0,0 +1,145 @@
+# == Class: openstack_project::jenkins
+#
+class openstack_project::jenkins (
+  $vhost_name = $::fqdn,
+  $jenkins_jobs_password = '',
+  $jenkins_jobs_username = 'gerrig', # This is not a typo, well it isn't anymore.
+  $jenkins_git_url = 'https://git.openstack.org/openstack-infra/jenkins-job-builder',
+  $jenkins_git_revision = 'master',
+  $manage_jenkins_jobs = true,
+  $ssl_cert_file = '',
+  $ssl_key_file = '',
+  $ssl_chain_file = '/etc/ssl/certs/intermediate.pem',
+  $ssl_cert_file_contents = '',
+  $ssl_key_file_contents = '',
+  $ssl_chain_file_contents = '',
+  $jenkins_ssh_private_key = '',
+  $zmq_event_receivers = [],
+  $sysadmins = [],
+  $project_config_repo = '',
+) {
+  include openstack_project
+
+  $iptables_rule = regsubst ($zmq_event_receivers, '^(.*)$', '-m state --state NEW -m tcp -p tcp --dport 8888 -s \1 -j ACCEPT')
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [80, 443],
+    iptables_rules6           => $iptables_rule,
+    iptables_rules4           => $iptables_rule,
+    sysadmins                 => $sysadmins,
+  }
+
+  # Set defaults here because they evaluate variables which you cannot
+  # do in the class parameter list.
+  if $ssl_cert_file == '' {
+    $prv_ssl_cert_file = "/etc/ssl/certs/${vhost_name}.pem"
+  }
+  else {
+    $prv_ssl_cert_file = $ssl_cert_file
+  }
+  if $ssl_key_file == '' {
+    $prv_ssl_key_file = "/etc/ssl/private/${vhost_name}.key"
+  }
+  else {
+    $prv_ssl_key_file = $ssl_key_file
+  }
+
+  class { '::jenkins::master':
+    vhost_name              => $vhost_name,
+    serveradmin             => 'webmaster@openstack.org',
+    logo                    => 'openstack.png',
+    ssl_cert_file           => $prv_ssl_cert_file,
+    ssl_key_file            => $prv_ssl_key_file,
+    ssl_chain_file          => $ssl_chain_file,
+    ssl_cert_file_contents  => $ssl_cert_file_contents,
+    ssl_key_file_contents   => $ssl_key_file_contents,
+    ssl_chain_file_contents => $ssl_chain_file_contents,
+    jenkins_ssh_private_key => $jenkins_ssh_private_key,
+    jenkins_ssh_public_key  => $openstack_project::jenkins_ssh_key,
+  }
+
+  jenkins::plugin { 'build-timeout':
+    version => '1.14',
+  }
+  jenkins::plugin { 'copyartifact':
+    version => '1.22',
+  }
+  jenkins::plugin { 'dashboard-view':
+    version => '2.3',
+  }
+  jenkins::plugin { 'envinject':
+    version => '1.70',
+  }
+  jenkins::plugin { 'gearman-plugin':
+    version => '0.1.1',
+  }
+  jenkins::plugin { 'git':
+    version => '1.1.23',
+  }
+  jenkins::plugin { 'greenballs':
+    version => '1.12',
+  }
+  jenkins::plugin { 'extended-read-permission':
+    version => '1.0',
+  }
+  jenkins::plugin { 'zmq-event-publisher':
+    version => '0.0.3',
+  }
+#  TODO(jeblair): release
+#  jenkins::plugin { 'scp':
+#    version => '1.9',
+#  }
+  jenkins::plugin { 'jobConfigHistory':
+    version => '1.13',
+  }
+  jenkins::plugin { 'monitoring':
+    version => '1.40.0',
+  }
+  jenkins::plugin { 'nodelabelparameter':
+    version => '1.2.1',
+  }
+  jenkins::plugin { 'notification':
+    version => '1.4',
+  }
+  jenkins::plugin { 'openid':
+    version => '1.5',
+  }
+  jenkins::plugin { 'postbuildscript':
+    version => '0.16',
+  }
+  jenkins::plugin { 'publish-over-ftp':
+    version => '1.7',
+  }
+  jenkins::plugin { 'simple-theme-plugin':
+    version => '0.2',
+  }
+  jenkins::plugin { 'timestamper':
+    version => '1.3.1',
+  }
+  jenkins::plugin { 'token-macro':
+    version => '1.5.1',
+  }
+
+  if $manage_jenkins_jobs == true {
+    class { 'project_config':
+      url  => $project_config_repo,
+    }
+
+    class { '::jenkins::job_builder':
+      url          => "https://${vhost_name}/",
+      username     => $jenkins_jobs_username,
+      password     => $jenkins_jobs_password,
+      git_revision => $jenkins_git_revision,
+      git_url      => $jenkins_git_url,
+      config_dir   => $::project_config::jenkins_job_builder_config_dir,
+      require      => $::project_config::config_dir,
+    }
+
+    file { '/etc/default/jenkins':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
+      source => 'puppet:///modules/openstack_project/jenkins/jenkins.default',
+    }
+  }
+}
---
+++ openstack_project/manifests/static.pp
@@ -0,0 +1,301 @@
+# == Class: openstack_project::static
+#
+class openstack_project::static (
+  $sysadmins = [],
+  $swift_authurl = '',
+  $swift_user = '',
+  $swift_key = '',
+  $swift_tenant_name = '',
+  $swift_region_name = '',
+  $swift_default_container = '',
+  $project_config_repo = '',
+) {
+
+  class { 'openstack_project::server':
+    iptables_public_tcp_ports => [22, 80, 443],
+    sysadmins                 => $sysadmins,
+  }
+
+  class { 'project_config':
+    url  => $project_config_repo,
+  }
+
+  include openstack_project
+  class { 'jenkins::jenkinsuser':
+    ssh_key => $openstack_project::jenkins_ssh_key,
+  }
+
+  include apache
+  include apache::mod::wsgi
+
+  a2mod { 'rewrite':
+    ensure => present,
+  }
+  a2mod { 'proxy':
+    ensure => present,
+  }
+  a2mod { 'proxy_http':
+    ensure => present,
+  }
+
+  if ! defined(File['/srv/static']) {
+    file { '/srv/static':
+      ensure => directory,
+    }
+  }
+
+  ###########################################################
+  # Tarballs
+
+  apache::vhost { 'tarballs.openstack.org':
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/tarballs',
+    require  => File['/srv/static/tarballs'],
+  }
+
+  file { '/srv/static/tarballs':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  ###########################################################
+  # CI
+
+  apache::vhost { 'ci.openstack.org':
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/ci',
+    require  => File['/srv/static/ci'],
+  }
+
+  file { '/srv/static/ci':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  ###########################################################
+  # Logs
+
+  apache::vhost { 'logs.openstack.org':
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/logs',
+    require  => File['/srv/static/logs'],
+    template => 'openstack_project/logs.vhost.erb',
+  }
+
+  apache::vhost { 'logs-dev.openstack.org':
+    port     => 80,
+    priority => '51',
+    docroot  => '/srv/static/logs',
+    require  => File['/srv/static/logs'],
+    template => 'openstack_project/logs-dev.vhost.erb',
+  }
+
+  file { '/srv/static/logs':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  file { '/srv/static/logs/robots.txt':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'puppet:///modules/openstack_project/disallow_robots.txt',
+    require => File['/srv/static/logs'],
+  }
+
+  package { 'keyring':
+    ensure   => 'latest',
+    provider => 'pip',
+  }
+
+  vcsrepo { '/opt/os-loganalyze':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://git.openstack.org/openstack-infra/os-loganalyze',
+    require  => Package['keyring'],
+  }
+
+  exec { 'install_os-loganalyze':
+    command     => 'python setup.py install',
+    cwd         => '/opt/os-loganalyze',
+    path        => '/bin:/usr/bin',
+    refreshonly => true,
+    subscribe   => Vcsrepo['/opt/os-loganalyze'],
+  }
+
+  file { '/etc/os_loganalyze':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Vcsrepo['/opt/os-loganalyze'],
+  }
+
+  file { '/etc/os_loganalyze/wsgi.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'www-data',
+    mode    => '0440',
+    content => template('openstack_project/os-loganalyze-wsgi.conf.erb'),
+    require => File['/etc/os_loganalyze'],
+  }
+
+  file { '/srv/static/logs/help':
+    ensure  => directory,
+    recurse => true,
+    purge   => true,
+    force   => true,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    source  => 'puppet:///modules/openstack_project/logs/help',
+    require => File['/srv/static/logs'],
+  }
+
+  file { '/usr/local/sbin/log_archive_maintenance.sh':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0744',
+    source => 'puppet:///modules/openstack_project/log_archive_maintenance.sh',
+  }
+
+  cron { 'gziprmlogs':
+    user        => 'root',
+    minute      => '0',
+    hour        => '7',
+    weekday     => '6',
+    command     => 'bash /usr/local/sbin/log_archive_maintenance.sh',
+    environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
+    require     => File['/usr/local/sbin/log_archive_maintenance.sh'],
+  }
+
+  ###########################################################
+  # Docs-draft
+
+  apache::vhost { 'docs-draft.openstack.org':
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/docs-draft',
+    require  => File['/srv/static/docs-draft'],
+  }
+
+  file { '/srv/static/docs-draft':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  file { '/srv/static/docs-draft/robots.txt':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'puppet:///modules/openstack_project/disallow_robots.txt',
+    require => File['/srv/static/docs-draft'],
+  }
+
+  class { 'openstack_project::pypi_mirror':
+    vhost_name => 'pypi.openstack.org',
+  }
+
+  # Legacy pypi mirror
+  file { '/srv/static/mirror/web/openstack':
+    ensure  => absent,
+    force   => true,
+  }
+
+  ###########################################################
+  # Security
+
+  apache::vhost { 'security.openstack.org':
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/security',
+    require  => File['/srv/static/security'],
+  }
+
+  file { '/srv/static/security':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  ###########################################################
+  # Governance
+
+  apache::vhost { 'governance.openstack.org':
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/governance',
+    require  => File['/srv/static/governance'],
+  }
+
+  file { '/srv/static/governance':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  ###########################################################
+  # Specs
+
+  apache::vhost { 'specs.openstack.org':
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/specs',
+    require  => File['/srv/static/specs'],
+  }
+
+  file { '/srv/static/specs':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  file { '/srv/static/specs/index.html':
+    ensure  => present,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    mode    => '0444',
+    source  => $::project_config::specs_index_file,
+    require => [File['/srv/static/specs'],
+                $::project_config::config_dir],
+  }
+
+  ###########################################################
+  # legacy summit.openstack.org site redirect
+
+  apache::vhost { 'summit.openstack.org':
+    port          => 80,
+    priority      => '50',
+    docroot       => 'MEANINGLESS_ARGUMENT',
+    template      => 'openstack_project/summit.vhost.erb',
+  }
+
+  ###########################################################
+  # legacy devstack.org site redirect
+
+  apache::vhost { 'devstack.org':
+    port          => 80,
+    priority      => '50',
+    docroot       => 'MEANINGLESS_ARGUMENT',
+    serveraliases => ['*.devstack.org'],
+    template      => 'openstack_project/devstack.vhost.erb',
+  }
+}
